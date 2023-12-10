e1a3ac8a37d1e200da6cedcfbb2074e33da851fb
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-11 02:41:37 +0300

    organize project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+import Contact from "./components/contact/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <Header />
+      <div className="divider" />
+      <Hero />
+      <div className="divider" />
+      <Main />
+      <div className="divider" />
+      <Contact />
+      <div className="divider" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
