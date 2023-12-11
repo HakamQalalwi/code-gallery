492d205a6f3ce38e3dbf30b4178f716efac9bf77
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-11 18:57:48 +0300

    Build and Design Header Section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import { useState } from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <header className=" flex">
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+        className="menu icon-menu flex"
+      ></button>
+      <div />
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articls</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="mode flex">
+        <span className="icon-moon-o"></span>
+      </button>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal">
+            <li>
+              <button
+                className="icon-clear"
+                onClick={() => setShowModal(false)}
+              />
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Articls</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">Uses</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
