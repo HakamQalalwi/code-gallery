93c919b71c4489d06a0b47171e1381b9e22d0249
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-12 16:28:57 +0300

    Build and Design Footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,25 @@
-import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Speaking</a>
+        </li>
+        <li>
+          <a href="">Uses</a>
+        </li>
+      </ul>
+      <p>2023 Spencer Sharp. All rights reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
