5c3bc4a825ab88c7ce34960ffe9471c563c4baf8
Hakam Qalalwi <hakamqalalwi@outlook.com>
2023-12-12 16:20:38 +0300

    Build and Design Contact Us section without left side animation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,35 @@
-import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Contact Us
+      </h1>
+      <p className="sub-title">
+        Contact us for more information and Get notified when I publish
+        something new
+      </p>
+
+      <div className="flex">
+        <form className="">
+          <div className="flex">
+            <label htmlFor="email">Email Address:</label>
+            <input required type="email" id="email" />
+          </div>
+
+          <div className="flex" style={{ marginTop: "24px" }}>
+            <label htmlFor="message">Your message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+
+          <button className="submit">Submit</button>
+        </form>
+        <div className="border animation">animation</div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
